bde4b11094d8f0e33ad320ca1e3fc0fff4b476ba
Michael Sankovich <misankovich@vassar.edu>
2020-05-27 15:46:26 -0400

    updated weather json

--- utils/csv.js
+++ utils/csv.js
@@ -3,22 +3,26 @@ const fs = require('fs');
 const path = require('path');
 const weatherConditions = require('../resources/weather-conditions.json');
 const obj = {};
-fs.createReadStream(path.join(__dirname, '..', 'resources', 'data.csv'))
+fs.createReadStream(path.join(__dirname, '..', 'resources', 'vhintl2.csv'))
   .pipe(csv())
-  .on('data', (row) => {
-    const rolls = row.Roll.split(' ');
-    const roll = rolls[rolls.length - 1];
-    obj[roll] = {
-      temp: row.Temp,
-      wind: row.Wind,
-      percipitation: row.Percipitation
-    }
+  .on('data', (row, a) => {
+    console.log(row)
+    // const rolls = row.Roll.split(' ');
+    // const roll = rolls[rolls.length - 1];
+    // obj[roll] = {
+    //   temp: row.Temp,
+    //   wind: row.Wind,
+    //   percipitation: row.Percipitation,
+    //   description: row.DESCRIPTION,
+    //   emoji: row.EMOJI
+    // }
   })
   .on('end', () => {
-    weatherConditions['inariko']['fall'] = obj;
-    weatherConditions['inariko']['spring'] = obj;
-    fs.writeFile(path.join(__dirname, '..', 'resources', 'weather-conditions.json'), JSON.stringify(weatherConditions, null, 2), (err) => {
-      console.log(err);
-    });
+    // weatherConditions['vhintl']['winter'] = obj;
+    // weatherConditions['vhintl']['fall'] = obj;
+    // weatherConditions['vhintl']['spring'] = obj;
+    // fs.writeFile(path.join(__dirname, '..', 'resources', 'weather-conditions.json'), JSON.stringify(weatherConditions, null, 2), (err) => {
+    //   console.log(err);
+    // });
     console.log('CSV file successfully processed');
   });
