e788e9ffc3c17dcf729ab50700241d42ae7f3065
Dani Sankovich <misankovich@gmail.com>
2020-08-22 12:24:13 -0400

    adding summer

--- utils/season.js
+++ utils/season.js
@@ -3,8 +3,11 @@ function getSeason() {
   const month = date.getMonth();
   const day = date.getDate();
 
-  if ((month > 2 && month < 8) || (month === 2 && day > 20) || (month === 8 && day < 21)) {
+  if ((month > 2 && month < 5) || (month === 2 && day > 20) || (month === 5 && day < 21)) {
     return 'spring';
+  }
+  if ((month > 5 && month < 8) || (month === 5 && day > 20) || (month === 8 && day < 21)) {
+    return 'summer';
   }
   if ((month > 8 && month < 11) || (month === 8 && day > 20) || (month === 11 && day < 21)) {
     return 'fall';
