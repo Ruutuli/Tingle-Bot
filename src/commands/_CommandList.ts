72010b51b09120a5ee86a472c8b8d463d7870ca5
Naomi Carrigan <nhcarrigan@gmail.com>
2022-02-18 14:36:13 -0800

    feat: remove roll command

--- src/commands/_CommandList.ts
+++ src/commands/_CommandList.ts
@@ -1,7 +1,6 @@
 import { forecast } from "./forecast";
-import { roll } from "./roll";
 
 /**
  * TODO: Migrate this to an automated import.
  */
-export const CommandList = [roll, forecast];
+export const CommandList = [forecast];
